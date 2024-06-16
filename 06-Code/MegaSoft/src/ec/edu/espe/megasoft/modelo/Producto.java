232f327bf64218354f3cdd77a5a95d6534938123
KennyJesusGavilanezOcampo <kennygavilanez2000@gmail.com>
2024-06-16 11:39:18 -0500

    update project

---
+++ 06-Code/MegaSoft/src/ec/edu/espe/megasoft/modelo/Producto.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.megasoft.modelo;
+
+/**
+ *
+ * @author Marco Chanataxi, Dev Dynasty, DCCO-ESPE
+ */
+public class Producto {
+    
+    private int id;
+    private String nombre;
+    private double precio;
+    private Categoria categoria;
+    private int stock;
+
+    public Producto(int id, String nombre, double precio, Categoria categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.stock = 0;
+     
+
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", categoria=" + categoria + ", stock=" + stock + '}';
+    }
+    
+}
---
+++ 06-Code/MegaSoft/src/ec/edu/espe/megasoft/vista/MegaSoft.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ec.edu.espe.megasoft.vista;
+
+import ec.edu.espe.megasoft.controlador.ControladorTienda;
+import ec.edu.espe.megasoft.modelo.Tienda;
+import java.io.IOException;
+
+/** 
+ *
+ * @author Marco Chanataxi, Dev Dynasty, DCCO-ESPE
+ */
+public class MegaSoft {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+   public static void main(String[] args) throws IOException {
+    Tienda tienda = new Tienda();
+    Menu menu = new Menu();
+    ControladorTienda controlador = new ControladorTienda(tienda, menu);
+
+    controlador.iniciar();
+    
+    
+        
+       
+    }
+}
+
